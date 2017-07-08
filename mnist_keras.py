afe65c46df4e632a7a6c002dfef2a2c2985c4136
willhyper <willhyper@gmail.com>
2017-07-07 23:28:27 -0700

    simplify mnist and add view_sample

--- mnist_keras.py
+++ mnist_keras.py
@@ -1,63 +1,47 @@
 from keras.datasets import mnist
+from keras.layers import Dense
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
-from keras.optimizers import RMSprop
 from keras.utils import to_categorical
+import numpy as np
 
-# the data, shuffled and split between train and test sets
+class codec:
+    @staticmethod
+    def enc(x):
+        return to_categorical(x, 10)
+    @staticmethod
+    def dec(y):
+        return np.argmax(y)
+
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 print("X_train shape:", X_train.shape)
 print("X_test shape:", X_test.shape)
 print("Data type:", X_train.dtype)
 
-# Exercise 1:
-# Reshape your data so that each image becomes a long vector
-N_train = X_train.shape[0]
-N_test = X_test.shape[0]
-X_train = X_train.reshape(N_train, 28*28)
-X_test = X_test.reshape(N_test, 28*28)
+X_train = X_train.reshape(X_train.shape[0], 28 * 28).astype('float32') / 255.0
+X_test = X_test.reshape(X_test.shape[0], 28 * 28).astype('float32') / 255.0
+yc_train = codec.enc(y_train)
+yc_test = codec.enc(y_test)
+print('yc_train.shape', yc_train.shape)
+print('yc_test.shape', yc_test.shape)
 
-# Exercise 2:
-# change the type of the input vectors to be 'float32'
-# and rescale them so that the values are between 0 and 1
-X_train = X_train.astype('float32')
-X_test = X_test.astype('float32')
-X_train /= 255.0
-X_test /= 255.0
-
-# Exercise 3:
-# convert class vectors to binary class matrices
-yc_train = to_categorical(y_train, 10)
-yc_test = to_categorical(y_test, 10)
-
-
-# Exercise 4:
-# https://keras.io/getting-started/sequential-model-guide/
-# Choose your architecture as you please
 model = Sequential()
-model.add(Dense(20, input_dim = 784, activation = 'relu'))
-model.add(Dropout(0.2))
-model.add(Dense(10, activation = 'softmax'))
+model.add(Dense(10, input_dim=784, activation='softmax'))
 model.summary()
 
-# Exercise 5:
-# Compile your model using an optimizer of your choice
-# make sure to display the accuracy metric
-# https://keras.io/optimizers/
-model.compile(optimizer='rmsprop',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Exercise 6:
-model.fit(X_train, yc_train, validation_split = 0.3, 
-        epochs=3, batch_size=128, verbose=0)
+model.fit(X_train, yc_train, validation_split=0.3, epochs=1)
 
+loss, acc = model.evaluate(X_test, yc_test, verbose=0)
+print('evaluate. loss, acc = ', loss, acc)
 
-# Exercise 7:
-loss, acc = model.evaluate(X_test, yc_test)
-print('loss', loss)
-print('accuracy', acc)
+def view_sample():
+    ind = np.random.randint(0, yc_test.shape[0])
+    Xind = X_test[np.array([ind])]
+    y_predict = codec.dec(model.predict(Xind))
+    y2 = model.predict_classes(Xind, verbose=0)
+    y_true = codec.dec(yc_test[ind])
+    print('y_true is ',y_true, '.learned machine predicts', y_predict, y2)
 
-# Bonus Exercise:
-# Modify the code to use a Convolutional Neural Network
-# Hints: you'll have to reshape your data to a 4D-array...
+for _ in range(10):
+    view_sample()
