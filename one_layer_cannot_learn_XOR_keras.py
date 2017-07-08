d13f7ab6c3aadb3caf53dfe9a5e6acd401a4bf30
willhyper <willhyper@gmail.com>
2017-07-07 23:28:48 -0700

    remove comment

--- one_layer_cannot_learn_XOR_keras.py
+++ one_layer_cannot_learn_XOR_keras.py
@@ -1,6 +1,7 @@
 '''
 objective: one layer nn cannot learn XOR. The activation is irrelevant
 '''
+
 print(__doc__)
 
 from keras.models import Sequential
@@ -10,14 +11,10 @@ import numpy as np
 X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y_train = np.array([0, 1, 1, 0]).T  # XOR
 
-# Reshape your data so that each image becomes a long vector
-N_train = X_train.shape[0]
-
 model = Sequential()
 model.add(Dense(1, input_dim=2))
 model.summary()
 
-# https://keras.io/optimizers/
 model.compile(optimizer='rmsprop',
               loss='mean_squared_error',
               metrics=['accuracy'])
