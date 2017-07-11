ebdc9508d112abd9f939a79a63a6ac510ca499c4
willhyper <willhyper@gmail.com>
2017-07-10 20:53:06 -0700

    assert result

--- one_layer_cannot_learn_XOR_keras.py
+++ one_layer_cannot_learn_XOR_keras.py
@@ -1,5 +1,10 @@
 '''
-objective: one layer nn cannot learn XOR. The activation is irrelevant
+objective: one layer nn cannot learn XOR.
+note:
+(1) no activation is needed for 1 layer nn as it is irrelevant
+(2) metrics=['accuracy'] is required, otherwise TypeError: 'numpy.float64' object is not iterable
+(3) (optimizer, loss) can be other combination
+
 '''
 
 print(__doc__)
@@ -15,15 +20,15 @@ model = Sequential()
 model.add(Dense(1, input_dim=2))
 model.summary()
 
-model.compile(optimizer='rmsprop',
+model.compile(optimizer='adam',
               loss='mean_squared_error',
               metrics=['accuracy'])
 
-for e in range(10):
-    epoch = 100
-    model.fit(X_train, y_train, epochs=epoch, verbose=0)
-    loss, acc = model.evaluate(X_train, y_train)
-    print(epoch * e, loss, acc)
+while True:
+    model.fit(X_train, y_train, epochs=1, verbose=0)
+    loss, acc = model.evaluate(X_train, y_train, verbose=0)
+    if acc >= 0.75: break
 
-print('well....accuracy will never be driven down. Math guaranteed.')
-print('Stopped iterations.')
+print('accuracy is up to 0.75. Math guaranteed.')
+y_pred_class = model.predict_classes(X_train)[:, 0] # [0] = best guess
+assert sum(y_pred_class == y_train) == 3
--- one_layer_learn_AND_keras.py
+++ one_layer_learn_AND_keras.py
@@ -1,6 +1,12 @@
 '''
-objective: one layer nn can learn AND. No activation needed.
+objective: one layer nn can learn AND.
+note:
+(1) no activation is needed for 1 layer nn as it is irrelevant
+(2) metrics=['accuracy'] is required, otherwise TypeError: 'numpy.float64' object is not iterable
+(3) (optimizer, loss) can be other combination
+
 '''
+
 print(__doc__)
 
 from keras.models import Sequential
@@ -10,25 +16,17 @@ import numpy as np
 X_train = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y_train = np.array([0, 0, 0, 1]).T  # AND
 
-# Reshape your data so that each image becomes a long vector
-N_train = X_train.shape[0]
-
 model = Sequential()
 model.add(Dense(1, input_dim=2))
 model.summary()
 
-# https://keras.io/optimizers/
-model.compile(optimizer='rmsprop',
-              loss='mean_squared_error',
-              metrics=['accuracy'])
+model.compile(optimizer='adam',loss='mean_squared_error',metrics=['accuracy'])
 
-epochs = 0
-acc = 0
-while acc < 0.99:
-    model.fit(X_train, y_train, epochs=100, verbose=0)
-    loss, acc = model.evaluate(X_train, y_train)
-    epochs += 500
-    print(epochs, loss, acc)
+while True:
+    model.fit(X_train, y_train, epochs=1, verbose=0)
+    loss, acc = model.evaluate(X_train, y_train, verbose=0)
+    if acc == 1.0: break
 
-w = model.get_weights()
-print('weights', w)
+y_pred_class = model.predict_classes(X_train)[:, 0] # [0] = best guess
+assert all(y_pred_class == y_train)
+print('learned AND.')
